f1a39ac634b55d55c9e4fb1d1381be93d8045b72
Simpi Arora <arorasimpi3@gmail.com>
2023-07-25 18:21:19 +0530

    Add files via upload

---
+++ bmiCalculator/bmiCalculator.js
@@ -0,0 +1,51 @@
+import { LightningElement } from 'lwc';
+
+export default class BmiCalculator extends LightningElement {
+    height = '';
+    weight = '';
+    bmiValue = '';
+    result = '';
+    inputHandler(event){
+        const{name,value} = event.target;
+        if(name == "height"){
+            this.height = value;
+        }
+        if(name == "weight"){
+            this.weight = value;
+        }
+
+        //this[name] = value; --> name is deprecated . can't use
+    }
+
+    submitHandler(event){
+        event.preventDefault();
+        console.log("height", this.height);
+        console.log("weight" , this.weight);
+        this.calculate();
+    }
+
+    calculate(){
+        let height = Number(this.height)/100;
+        let bmi = Number(this.weight) / (height * height);
+        console.log(bmi);
+        this.bmiValue = Number(bmi.toFixed(2));
+
+        if(this.bmiValue < 18.5){
+            this.result = "Underweight";
+        }else if(this.bmiValue >= 18.5 && this.bmiValue <25){
+            this.result = "Healthy";
+        }else if(this.bmiValue >=25 && this.bmiValue <30){
+            this.result = "Overweight";
+        }else{
+            this.result="Obese";
+        }
+    }
+
+    recalculate(){
+        this.height = '';
+        this.weight = '';
+        this.bmiValue = '';
+        this.result = '';
+    }
+    
+}
